b8432248914e74f6f19fec9ffd536aa2269af7eb
garvv29 <garv.choure2023@ssipmt.com>
2026-07-10 17:40:33 +0530

    made a browser renderer class that can start a headless chrome process, connect to it via CDP, and render a webpage to HTML

---
+++ include/browser/browserrenderer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+
+#include "browser/ChromeProcess.h"
+#include "browser/HTTPClient.h"
+#include "browser/CDPconn.h"
+
+class BrowserRenderer
+{
+public:
+
+    BrowserRenderer();
+
+    ~BrowserRenderer();
+
+    bool start(bool headless = true);
+
+    void stop();
+
+    std::string render(const std::string& url);
+
+private:
+
+    ChromeProcess chrome_;
+
+    HTTPClient http_;
+
+    CDPConnection cdp_;
+
+    bool initialized_ = false;
+    bool initializeCDP();
+};
---
+++ src/browser/browserrenderer.cpp
@@ -0,0 +1,91 @@
+#include "browser/BrowserRenderer.h"
+
+#include <windows.h>
+
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+BrowserRenderer::BrowserRenderer()
+{
+}
+
+BrowserRenderer::~BrowserRenderer()
+{
+    stop();
+}
+
+bool BrowserRenderer::initializeCDP()
+{
+    std::string response =
+        http_.get(
+            L"127.0.0.1",
+            9222,
+            L"/json/version");
+
+    json version =
+        json::parse(response);
+
+    std::string websocketURL =
+        version["webSocketDebuggerUrl"];
+
+    if(!cdp_.connect(websocketURL))
+        return false;
+
+    if(!cdp_.createPage())
+        return false;
+
+    if(!cdp_.enablePage())
+        return false;
+
+    if(!cdp_.enableRuntime())
+        return false;
+
+    return true;
+}
+
+bool BrowserRenderer::start(bool headless)
+{
+    if(initialized_)
+        return true;
+
+    if(!chrome_.start(headless))
+        return false;
+
+    if(!initializeCDP())
+        return false;
+
+    initialized_ = true;
+
+    return true;
+}
+
+void BrowserRenderer::stop()
+{
+    if(!initialized_)
+        return;
+
+    cdp_.disconnect();
+
+    chrome_.stop();
+
+    initialized_ = false;
+}
+
+std::string BrowserRenderer::render(
+    const std::string& url)
+{
+    if(!initialized_)
+    {
+        if(!start())
+            return "";
+    }
+
+    if(!cdp_.navigate(url))
+        return "";
+
+    cdp_.waitForLoad();
+
+    return cdp_.getHTML();
+}
+
